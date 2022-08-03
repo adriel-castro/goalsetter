ebb58611d3eed032a8fa6ae1d3319b7cdeef94b6
adriel-dev <adriel27castro@gmail.com>
2022-08-04 00:18:30 +0800

    frontend pages and components initial setup

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <div>
+          <h2>Login</h2>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            onChange={onChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            onChange={onChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
